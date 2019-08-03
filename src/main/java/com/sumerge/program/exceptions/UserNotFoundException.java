ac1aeb528897ae6e78837bf2bf9035ab2d3ee589
m-ohamed <mohamed.adel-ibrahim@student.guc.edu.eg>
2019-08-03 14:12:04 +0200

    finished and tested groups exception handling

---
+++ src/main/java/com/sumerge/program/exceptions/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.sumerge.program.exceptions;
+
+public class UserNotFoundException extends Exception
+{
+    public UserNotFoundException(String message)
+    {
+        super(message);
+    }
+}
